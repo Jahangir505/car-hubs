2abf50aac3a2b0a503745bb0a9010519dd787b58
Jahangir Hossain <jahangir758505@gmail.com>
2023-08-16 23:27:29 +0600

    Initial Commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import CarCard from "./CarCard";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CustomFilter from "./CustomFilter";
+
+
+export {
+    Hero,
+    CustomButton,
+    CarCard,
+    Footer,
+    Navbar,
+    SearchBar,
+    SearchManufacturer,
+    CustomFilter,
+}
